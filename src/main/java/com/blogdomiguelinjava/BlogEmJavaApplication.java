a9050253321d6f82088d26a9c7383faedfe5d9d2
omiguelgermano <miguelgermano86@gmail.com>
2023-10-05 20:40:05 -0300

    Primeiro push do projeto.

---
+++ src/main/java/com/blogdomiguelinjava/BlogEmJavaApplication.java
@@ -0,0 +1,13 @@
+package com.blogdomiguelinjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogEmJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogEmJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blogdomiguelinjava/BlogEmJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogdomiguelinjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogEmJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
