eada874dbbf3a00e6e2636d9757067db78a28703
omiguelgermano <miguelgermano86@gmail.com>
2023-10-05 21:30:40 -0300

    Criação dos atributos da Endidades Usuário.

--- src/main/java/com/blogdomiguelinjava/model/Usuario.java
+++ src/main/java/com/blogdomiguelinjava/model/Usuario.java
@@ -1,4 +1,31 @@
 package com.blogdomiguelinjava.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_usuarios")
 public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idUsuario;
+
+    @Column(nullable = false, length = 50)
+    private String nome;
+
+    @Column(nullable = false, length = 150)
+    private String sobrenome;
+
+    @Column(nullable = false, length = 100)
+    private String email;
+
+    @Column(nullable = false, length = 20)
+    private String senha;
+
 }
