efaa16d88b4081b09883dd7a5b37fe682c155674
omiguelgermano <miguelgermano86@gmail.com>
2023-10-05 21:40:21 -0300

    Criação dos atributos da Endidade Post.

--- src/main/java/com/blogdomiguelinjava/model/Post.java
+++ src/main/java/com/blogdomiguelinjava/model/Post.java
@@ -1,4 +1,33 @@
 package com.blogdomiguelinjava.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_posts")
 public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idPost;
+
+    @Column(nullable = false, length = 255)
+    private String titulo;
+
+    @Lob
+    @Column(nullable = false)
+    private String conteudo;
+
+    @Column
+    private Date email;
+
+    //Id autor
+
 }
