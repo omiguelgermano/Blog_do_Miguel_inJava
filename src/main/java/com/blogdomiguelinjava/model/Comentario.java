2d7daa4362705d0b30ddd0e9e4a2ad000c3e089a
omiguelgermano <miguelgermano86@gmail.com>
2023-10-05 21:44:37 -0300

    Criação dos atributos da Endidade Comentarios.

--- src/main/java/com/blogdomiguelinjava/model/Comentario.java
+++ src/main/java/com/blogdomiguelinjava/model/Comentario.java
@@ -1,4 +1,30 @@
 package com.blogdomiguelinjava.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_comentarios")
 public class Comentario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idComentario;
+
+    @Lob
+    @Column(nullable = false)
+    private String texto;
+
+    @Column
+    private Date dataDeCriacao;
+
+    //idAutor
+    //idPost
 }
