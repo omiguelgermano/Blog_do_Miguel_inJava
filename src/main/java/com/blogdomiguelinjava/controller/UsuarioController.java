468c22936556433a57b2ce28ad314ce23ef39358
omiguelgermano <miguelgermano86@gmail.com>
2023-10-06 16:48:56 -0300

    Definição do usuário Controller.

---
+++ src/main/java/com/blogdomiguelinjava/controller/UsuarioController.java
@@ -0,0 +1,71 @@
+package com.blogdomiguelinjava.controller;
+
+import com.blogdomiguelinjava.model.Usuario;
+import com.blogdomiguelinjava.repository.UsuarioRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(value = "/usuarios")
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @PostMapping
+    public Usuario cadastarUsuario(@Valid @RequestBody Usuario idUsuario){
+        return usuarioRepository.save(idUsuario);
+    }
+
+    @GetMapping
+    public List<Usuario> listarAutor(){
+        return usuarioRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Usuario>> obterUsuarioPeloId(@PathVariable("id") Long id) {
+        Optional<Usuario> usuarioExistente = usuarioRepository.findById(id);
+
+        if (usuarioExistente.isPresent()) {
+            return ResponseEntity.status(HttpStatus.OK).body(usuarioExistente);
+        }
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Usuario> atualizarUsuario(@PathVariable("id") Long id, @RequestBody Usuario usuario) {
+        Optional<Usuario> usuarioExistente = usuarioRepository.findById(id);
+
+        if (usuarioExistente.isPresent()) {
+            Usuario usuarioObj = usuarioExistente.get();
+
+            usuarioObj.setNome(usuario.getNome());
+            usuarioObj.setSobrenome(usuario.getSobrenome());
+            usuarioObj.setEmail(usuario.getEmail());
+            usuarioObj.setSenha(usuario.getSenha());
+
+            return ResponseEntity.status(HttpStatus.OK).body(usuarioRepository.save(usuarioObj));
+        }
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarUsuarioPeloId(@PathVariable("id") Long id) {
+        Optional<Usuario> usuarioExistente = usuarioRepository.findById(id);
+
+        if (usuarioExistente.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        usuarioRepository.deleteById(id);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+}
